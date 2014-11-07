b00586bb1185b2cffb1b69ff68ac69be51309455
pacogarat <paco@pacogarat.com>
2014-11-07 17:56:18 +0100

    init

---
+++ app/config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "miscodigos"
+set :domain,      "pacogarat.com"
+set :deploy_to,   "/var/www/miscodigos"
+set :app_path,    "app"
+set :user,        "pacogarat"
+
+set :use_composer, true
+set :update_vendors, true
+
+default_run_options[:pty] = true
+
+set :repository,  "git@github.com:pacogarat/miscodigos.git"
+set :scm,         :git
+set :deploy_via,  :rsync_with_remote_cache
+
+after "deploy",   "deploy:cleanup"
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+# logger.level = Logger::MAX_LEVEL
