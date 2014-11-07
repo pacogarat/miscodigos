ea5087c07d60e1520b901408dd52431b49025e2c
pacogarat <paco@pacogarat.com>
2014-11-07 21:30:44 +0100

    fix

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -2,17 +2,19 @@ set :application, "miscodigos"
 set :domain,      "pacogarat.com"
 set :deploy_to,   "/var/www/miscodigos"
 set :app_path,    "app"
+
 set :user,        "pacogarat"
 set :use_sudo,     false
+
 set :use_composer, true
 set :update_vendors, true
-set :branch,      "master"
 
 default_run_options[:pty] = true
 
 set :repository,  "git@github.com:pacogarat/miscodigos.git"
 set :scm,         :git
-set :deploy_via,  :rsync_with_remote_cache
+set :branch,      "master"
+set :deploy_via,  :remote_cache
 
 after "deploy",   "deploy:cleanup"
 
@@ -24,4 +26,4 @@ role :app,        domain, :primary => true       # This may be the same as your 
 set  :keep_releases,  3
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
---
+++ src/PG/Mcd/EntityBundle/Entity/UserRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PG\Mcd\EntityBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+    
+}
